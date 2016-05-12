0c9876816568a50f88a5c03f75dfe47d910ad252
Aaron Greenwald <aarong@wix.com>
2016-05-12 15:27:33 +0300

    first attempt at no-inline-styles rule

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 module.exports = {
   rules: {
     'no-unused-styles': require('./lib/rules/no-unused-styles'),
+    'no-inline-styles': require('./lib/rules/no-inline-styles'),
     'split-platform-components': require('./lib/rules/split-platform-components')
   },
   rulesConfig: {
     'no-unused-styles': 0,
+    'no-inline-styles': 0,
     'split-platform-components': 0
   }
 };
